3eed1effaee641928a65bce672484b7af1fa9dff
katie <kholland210@gmail.com>
2021-05-05 09:09:48 -0400

    added tests

--- htmlgreeter.test.ts
+++ htmlgreeter.test.ts
@@ -5,4 +5,12 @@ describe("html greeter test", function () {
         let htmlGreeter: HtmlGreeter = new HtmlGreeter("Hi", "h1");
         expect(htmlGreeter.greet("Katie")).toBe("<h1>Hi, Katie!</h1>");
     });
+    test("html greeter test 2", () => {
+        let htmlGreeter: HtmlGreeter = new HtmlGreeter("Hello", "h2");
+        expect(htmlGreeter.greet("Katie")).toBe("<h2>Hello, Katie!</h2>");
+    });
+    test("html greeter test 3", () => {
+        let htmlGreeter: HtmlGreeter = new HtmlGreeter("Welcome", "h3");
+        expect(htmlGreeter.greet("friends")).toBe("<h3>Welcome, friends!</h3>");
+    });
 });
